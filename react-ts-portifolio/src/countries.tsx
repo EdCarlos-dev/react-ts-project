265eb453d0c2bf870f4c9f3923e951edf6a1afaa
EdCarlos-dev <bio_edc@hotmail.com>
2024-01-03 17:04:52 -0300

    update style

---
+++ react-ts-portifolio/src/countries.tsx
@@ -0,0 +1,37 @@
+
+
+// const countries = [
+//   { name: 'Finland', city: 'Helsinki' },
+//   { name: 'Sweden', city: 'Stockholm' },
+//   { name: 'Denmark', city: 'Copenhagen' },
+//   { name: 'Norway', city: 'Oslo' },
+//   { name: 'Iceland', city: 'Reykjavík' },
+// ]
+
+// // Country component
+// function Country({ country: { name, city } }) {
+//     return (
+//         <div>
+//             <h1>{name}</h1>
+//             <small>{city}</small>
+//         </div>
+//     )
+// }
+
+// // countries component
+// function Countries ({ countries }) => {
+//   const countryList = countries.map((country) => (
+//     <Country key={country.name} country={country} />
+//   ))
+//   return <div>{countryList}</div>
+// }
+// function Contr () (
+//   <div className='container'>
+//     <div>
+//       <h1>Countries List</h1>
+//       <Countries countries={countries} />
+//     </div>
+//   </div>
+// )
+
+// export default Contr
