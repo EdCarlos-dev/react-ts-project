c67cc30ac82326a08e547d0ae929555c33ffe61e
EdCarlos-dev <bio_edc@hotmail.com>
2024-01-03 17:18:39 -0300

    update app structure

--- react-ts-portifolio/src/App.tsx
+++ react-ts-portifolio/src/App.tsx
@@ -45,6 +45,14 @@ const techLogos = (
     <img className='techimage' src={cssLogo} alt='ReactLogo' />
     <img className='techimage' src={javascriptLogo} alt='ReactLogo' />
     
+    <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo vite" alt="Vite logo" />
+    </a>
+    <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+    </a>
+    <h1>Vite + React</h1>
+    
   </div>
 )
 
@@ -90,35 +98,20 @@ function App() {
    <div>
     <Header/>
     <UserCard/>
-    {skilsFormatted}
 
       <div className='skils-div'>
+      {skilsFormatted}
+      </div>
       
-      
+      {techLogos}
+ 
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <Button/>
+        </div>
 
-      </div>
-      <div>
-        {techLogos}
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <Button/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
        </div>
       <Footer/>
     </>
--- react-ts-portifolio/src/footer.tsx
+++ react-ts-portifolio/src/footer.tsx
@@ -6,6 +6,15 @@ function Footer () {
 
   return (
     <footer>
+      <div>
+      <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      </div>
+      
       <div className='footer-wrapper'>
         <p>{copyRight}</p>
       </div>
